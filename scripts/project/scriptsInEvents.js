a406e2edb226a4626ee32c3a6ab9b7e42167a3ea
Khaled Felfal <felfal201@gmail.com>
2024-07-05 20:53:59 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,20 @@
+
+
+
+const scriptsInEvents = {
+
+	async MainmenuEs_Event8_Act1(runtime, localVars)
+	{
+		const arabicRegex = /[\u0600-\u06FF]/; // Range of Arabic Unicode characters
+		const textToCheck = localVars["text"]; // Replace with the text you want to check
+		
+		const hasArabic = arabicRegex.test(textToCheck);
+		const returnValue = hasArabic ? 1 : 0;
+		
+		runtime.setReturnValue(returnValue);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
